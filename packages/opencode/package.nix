6d5c412f9b6de76b9babc7e809408c4d7f6b8e03
ErikBPF <26701869+ErikBPF@users.noreply.github.com>
2026-08-13 10:44:15 +0000

    chore: update packaged upstream

--- packages/opencode/package.nix
+++ packages/opencode/package.nix
@@ -21,7 +21,7 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "opencode";
-  version = "1.18.16";
+  version = "1.18.18";
 
   __structuredAttrs = true;
   strictDeps = true;
@@ -30,7 +30,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     owner = "anomalyco";
     repo = "opencode";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-AP2W443Zk/X8j6BWfMgAEbR4BQiJgnPpr1OG6JWIprE=";
+    hash = "sha256-rDVcv8j9KghTDwooPYriTloOMgTyVutud7xKLG2mTmk=";
   };
 
   node_modules = stdenvNoCC.mkDerivation {
@@ -85,7 +85,7 @@ stdenvNoCC.mkDerivation (finalAttrs: {
     # NOTE: Required else we get errors that our fixed-output derivation references store paths
     dontFixup = true;
 
-    outputHash = "sha256-3BrsXBSchRXPoqJif7GXSLD8NW2VuSIL0kgOj8IUmq8=";
+    outputHash = "sha256-DrvEnHaxG6hFWcsmVyGiKTwAT2V5wpVP0GyxZr+KXXE=";
     outputHashAlgo = "sha256";
     outputHashMode = "recursive";
   };
